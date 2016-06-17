b87510dd85f1745028afaaea725d9507dd99b8f5
juanjocruz <juanjojac@gmail.com>
2016-06-17 14:06:33 +0200

    Mi primer commit

---
+++ app/src/main/java/com/example/dell_09_027_pc/proyectonuevo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dell_09_027_pc.proyectonuevo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
